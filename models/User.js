9057b489fadef7c6d63767695e37eb0bfad36d2e
nidhi <inidhi99@gmail.com>
2022-11-08 17:36:40 -0500

    added user.js model

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+// Create a new Sequelize model for user
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+User.init(
+    // Define fields/columns on model
+  // An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // prevents duplicate email addresses in DB
+      unique: true,
+      //validations 
+      validate: {
+        isAlphanumeric: true,
+        min: 2,
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  //hashing the password 
+  {
+    hooks: {
+      // Use the beforeCreate hook to work with data before a new instance is created
+      beforeCreate: async (newUserData) => {
+        // In this case, we are taking the user's email address
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    // Link to database connection
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user', 
+  }
+);
+
+module.exports = User;
