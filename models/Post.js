296234da1183f2eaebdd8b013259cc28cf22e8d9
nidhi <inidhi99@gmail.com>
2022-11-08 21:26:50 -0500

    added post and comment models

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    // for each paost
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // particular user id of the post
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      reference: {
+        model: "User",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "posts",
+  }
+);
+
+module.exports = Post;
--- models/User.js
+++ models/User.js
@@ -29,6 +29,14 @@ User.init(
         min: 2,
       }
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
